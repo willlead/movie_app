8c906f7fe747769dfe8e24f4fe0df715965637b4
willlead <willlead@naver.com>
2020-05-03 20:44:16 +0900

    Updates

---
+++ precache-manifest.6802e0354d25883b285eb5bef62edf9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a8c04010109ace411f1ac2f6f277a7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ddc0b41e3db6e8fdb9f8",
+    "url": "/movie_app/static/css/main.10f23381.chunk.css"
+  },
+  {
+    "revision": "f66daf3eed1d27073954",
+    "url": "/movie_app/static/js/2.6faa7bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.6faa7bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc0b41e3db6e8fdb9f8",
+    "url": "/movie_app/static/js/main.c88583a8.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
