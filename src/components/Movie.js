a1caf507f38a1e0fd538c987c7f2d83077a1e1cb
willlead <willlead@naver.com>
2020-05-03 22:27:56 +0900

    movie-detail => movie/:id  로 변경

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,31 +4,36 @@ import "./Movie.css"
 import { Link } from "react-router-dom";
 
 
-function Movie({ year, title, summary, poster, genres }) {
-    return <Link to = {{
-        pathname: '/movie-detail',
-        state:{
-            year,
-            title,
-            summary,
-            poster,
-            genres
-        }
-    }}>
-        <div className="movie">
+function Movie({ id, year, title, summary, poster, genres }) {
+    return (<div className="movie">
+        <Link 
+            to={{
+            pathname: '/movie/${id}',
+            state: {
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}>
             <img src={poster} alt={title} title={title} />
-            <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
-                    {genres.map((genre, index) =>
-                        <li key={index} className="geners__genre">{genre}</li>)}
-                </ul>
-                <p className="movie__summary">{summary}</p>
+            <div className="movie">
+                
+                <div className="movie__data">
+                    <h3 className="movie__title">{title}</h3>
+                    <h5 className="movie__year">{year}</h5>
+                    <ul className="movie__genres">
+                        {genres.map((genre, index) =>
+                            <li key={index} className="geners__genre">{genre}</li>)}
+                    </ul>
+                    <p className="movie__summary">{summary}</p>
 
+                </div>
             </div>
-        </div>
-    </Link>
+        </Link>
+    </div>
+    );
 }
 
 Movie.prototype = {
